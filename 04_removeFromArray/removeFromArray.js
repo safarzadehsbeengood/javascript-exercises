7d3c50281bafd47ba330d3778f479f2d054aa8c5
Ryan Safa <safarzadehryan@gmail.com>
2024-03-27 23:54:39 -0700

    1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...targets) {
+    for (target of targets) {
+    for (let i = arr.length-1; i >= 0; i--) {
+        if (arr[i] === target) {
+            arr.splice(i, 1);
+        }
+    }
+}
+    return arr;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(t) {
+  return Math.round(((t-32)/1.8)*10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(t) {
+  return Math.round((t*1.8+32)*10) / 10;
 };
 
 // Do not edit below this line
